505f46659b22cacf1f0048893c87f0caca3e617a
Le Tran Anh Khoi <kais4r.dev@gmail.com>
2024-08-30 15:45:33 +0700

    Setup basic api and connection to mongo db atlas

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import mongoose from "mongoose";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// Use CORS middleware
+app.use(cors());
 
-export default app
+const mongoURI =
+  "mongodb+srv://khoi:l4YUX90EBHnL5TFj@dluunderground.obcrv.mongodb.net/?retryWrites=true&w=majority&appName=DLUUnderground";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+app.notFound((c) => {
+  return c.text("404 not found error check your api", 404);
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  return c.text("Nuh uh error code 500", 500);
+});
+
+app.get("/", (c) => {
+  return c.text("Hello! Welcome to DLU underground");
+});
+
+export default app;
